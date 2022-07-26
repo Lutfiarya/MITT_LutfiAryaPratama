d1fd7b73f41d7d1eadcd4c83c900bb6f7bc10d0e
Lutfi Arya <Lutfi Arya@DESKTOP-SQJ3PP5>
2022-07-26 22:57:42 +0700

    Add project files.

---
+++ MITT/Controllers/UserProfileController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MITT.Models;
+
+namespace MITT.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserProfileController : Controller
+    {
+        private MasterContext _context = new MasterContext();
+
+        [HttpGet]
+        public List<UserProfile> GetAll()
+        {
+            return _context.UserProfile.ToList();
+        }
+
+
+        [HttpPost("Register")]
+        public IActionResult Create([FromBody] UserProfile userProfile)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.UserProfile.Add(userProfile);
+            _context.SaveChanges();
+
+            return View("success");
+        }
+
+        [HttpPut("Register/{Id}")]
+        public IActionResult Update([FromBody] UserProfile userProfile, int Id)
+        {
+            if (userProfile.userProfileId != Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(userProfile).State = EntityState.Modified;
+            _context.SaveChanges();
+            return Ok();
+        }
+
+    }
+}
---
+++ MITT/Models/UserProfile.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MITT.Models
+{
+    [Table("userprofile")]
+    public class UserProfile
+    {
+        [Key]   
+        public int userProfileId { get; set; } 
+        public string username { get; set; }
+        public string name { get; set; }
+        public string address { get; set; }
+        public DateTime bod { get; set; }
+
+        public string email { get; set; }
+
+    }
+}
